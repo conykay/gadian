32400cce9ac1cb26c2474cb4dd38bb4e83c5e98a
Conelius <ceekayconelius@gmail.com>
2022-07-01 10:29:33 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/gadian/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gadian
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/methods/onboarding_info.dart
@@ -0,0 +1,7 @@
+class OnboardingInfo {
+  final String title;
+  final String description;
+  final String image;
+
+  OnboardingInfo(this.title, this.description, this.image);
+}
