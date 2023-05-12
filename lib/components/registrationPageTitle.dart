510f5de0ae37c316dbefa69d6ac8487f633e4961
Conelius <ceekayconelius@gmail.com>
2023-05-12 10:13:28 +0300

    registrationPageTitle.dart changing icon and background color depending on mode.

--- lib/components/registrationPageTitle.dart
+++ lib/components/registrationPageTitle.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 
 Container kBuildPageTitle(
     BuildContext context, String title, String info, IconData icon) {
+  Brightness mode = Theme.of(context).brightness;
+  Color iconDarkColor = Theme.of(context).colorScheme.secondary;
+  Color iconLightColor = Theme.of(context).colorScheme.primary;
   return Container(
     decoration: BoxDecoration(color: Colors.redAccent.withOpacity(0.05)),
     child: Row(
@@ -12,12 +15,14 @@ Container kBuildPageTitle(
           child: Container(
             padding: const EdgeInsets.all(5),
             decoration: BoxDecoration(
-                color: Theme.of(context).hintColor.withOpacity(0.2),
+                color: mode == Brightness.light
+                    ? iconLightColor.withOpacity(0.2)
+                    : iconDarkColor.withOpacity(0.2),
                 borderRadius: BorderRadius.circular(10)),
             child: Icon(
               icon,
               size: 50,
-              color: Theme.of(context).hintColor,
+              color: mode == Brightness.light ? iconLightColor : iconDarkColor,
             ),
           ),
         ),
